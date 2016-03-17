d059e05b2d4af484d0970f0ec189f3eb77014704
Phi Long Tran <philongtran@outlook.de>
2016-03-17 13:57:30 +0100

    initial commit

---
+++ FastPipe/src/FastPipe.java
@@ -0,0 +1,69 @@
+// Primer der "Pipe" zur Primzahlenaussiebung
+public class FastPipe extends Thread {
+	private int p; // die Primzahl dieses Primers
+	private FastPipe next; // der nächste Primer in der "Pipe"
+
+	FastPipe(int prime) { // Konstuktor
+		super("Primer-" + prime); // Name eintragen
+		p = prime; // Primzahl eintragen
+		this.start(); // Thread sofort starten
+	}
+
+	// ... weitere Variablen und Methoden ...
+	public static void main(String args[]) {
+		FastPipe first = new FastPipe(1); // ersten Primer : 2
+		for (int i = 2; i <= 100; i++) {
+			first.send(i);
+		}
+		; // weitere
+
+		first.send(0); // Abbruchmitteilung
+
+		System.out.println(currentThread() + " main beendet");
+	}
+
+	@Override
+	public void run() { // Die Arbeitsmethode des Primers
+		// ... ist nicht synchronisiert
+
+		System.out.println(p);
+		while (true) { // Endlos-Schleife
+			int n = receive(); // Lese-Versuch
+			if (n == 0) {// wenn n=0: Ende
+				if (next != null)
+					System.out.println("close");
+				next.send(n);// auch von next
+				break; // Ende whileloop
+			} else {
+				next = new FastPipe(n); // Primzahl!
+			} // sonst: n nicht prim
+		}
+	}
+
+	private int buffer = -1; // Puffer zum Senden & Empfangen
+	// wenn < 0: leer
+
+	synchronized void send(int i) { // Sperre erlangen
+		try {
+			while (buffer >= 0)
+				wait();// warten bis Puffer frei
+			buffer = i; // Puffer füllen
+			notify(); // Empfänger benachrichtigen
+		} catch (InterruptedException e) {
+		}
+	}
+
+	synchronized int receive() { // Sperre erlangen
+		int result = 0;
+		try {
+			while ((result = buffer) < 0)
+				wait();// warten bis Puffer
+			// voll
+			buffer = -1; // Puffer leeren
+			notify(); // Sender
+			// benachrichtigen
+		} catch (InterruptedException e) {
+		}
+		return result;
+	}
+}
