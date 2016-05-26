f52ed7fc8099e0f884e2e243af3076e29a9e4bee
Manuel <wtfjoke@livenet.ch>
2016-05-27 00:49:06 +0200

    use class borders for calculating the borders of a cell
    - remove unused stuff

--- GameOfLife/src/Game.java
+++ GameOfLife/src/Game.java
@@ -6,14 +6,9 @@ import javax.swing.Timer;
 public class Game extends Observable {
 
 	private Board board;
-	private Position leftUpperCorner, upperCorner, rightUpperCorner, leftCorner, rightCorner, leftBottomCorner,
-			bottomCorner, rightBottomCorner;
-	private Position[] actualBorders = new Position[8];
-	// private int livingCells;
 	public GoLChildWindow golCildWindow;
 	private Timer timer;
 	private boolean start = true;
-	// timer which sends an action every 1sec.
 	private int delay = 500; // milliseconds
 	public int golWindowNumber = GameOfLife.GOLWINDOWNUMBER;
 
@@ -36,9 +31,7 @@ public class Game extends Observable {
 		for (int y = 0; y < board.getSizeY(); y++) {
 			for (int x = 0; x < board.getSizeX(); x++) {
 				int livingCells = 0;
-				calculateBorders(x, y);
-				for (int i = 0; i < actualBorders.length; i++) {
-					Position border = actualBorders[i];
+				for (Position border : Borders.of(board, x, y).getPositions()) {
 					if (board.getStatus(border.getPositionX(), border.getPositionY())) {
 						livingCells++;
 					}
@@ -62,50 +55,6 @@ public class Game extends Observable {
 		}
 		setChanged();
 		notifyObservers();
-	}
-
-	private void calculateBorders(int x, int y) {
-		int newXLeft, newYUp, newXRight, newYDown;
-
-		if (x - 1 < 0) {
-			newXLeft = board.getSizeX() - 1;
-		} else {
-			newXLeft = x - 1;
-		}
-		if (y - 1 < 0) {
-			newYUp = board.getSizeY() - 1;
-		} else {
-			newYUp = y - 1;
-		}
-
-		if (x + 1 >= board.getSizeX()) {
-			newXRight = 0;
-		} else {
-			newXRight = x + 1;
-		}
-		if (y + 1 >= board.getSizeY()) {
-			newYDown = 0;
-		} else {
-			newYDown = y + 1;
-		}
-
-		leftUpperCorner = new Position(newXLeft, newYUp);
-		upperCorner = new Position(x, newYUp);
-		rightUpperCorner = new Position(newXRight, newYUp);
-		leftCorner = new Position(newXLeft, y);
-		rightCorner = new Position(newXRight, y);
-		leftBottomCorner = new Position(newXLeft, newYDown);
-		bottomCorner = new Position(x, newYDown);
-		rightBottomCorner = new Position(newXRight, newYDown);
-
-		actualBorders[0] = leftUpperCorner;
-		actualBorders[1] = upperCorner;
-		actualBorders[2] = rightUpperCorner;
-		actualBorders[3] = leftCorner;
-		actualBorders[4] = rightCorner;
-		actualBorders[5] = leftBottomCorner;
-		actualBorders[6] = bottomCorner;
-		actualBorders[7] = rightBottomCorner;
 	}
 
 	public int getSizeX() {
