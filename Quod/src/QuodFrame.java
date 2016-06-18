b91cbedb6b95ab3d857a4b5228efe2d48a1ada33
Phi Long Tran <philongtran@outlook.de>
2016-06-18 09:31:39 +0200

    add coordinates to button ActionCommand

--- Quod/src/QuodFrame.java
+++ Quod/src/QuodFrame.java
@@ -11,10 +11,22 @@ public class QuodFrame extends JInternalFrame {
   public QuodFrame() {
     super("Quod", true, true);
     JPanel gameBoard = new JPanel();
+    JButton[][] buttons = new JButton[11][11];
     int boardSize = QuodGame.getInstance().getBoard().getSize();
     gameBoard.setLayout(new GridLayout(boardSize, boardSize, 1, 1));
+    int x = 0;
+    int y = 0;
     for (int i = 1; i < boardSize * boardSize; i++) {
-      gameBoard.add(createCell(String.valueOf(i)));
+      JButton tempButton = createCell(String.valueOf(i));
+      tempButton.setActionCommand(x + "," + y);
+      gameBoard.add(tempButton);
+      buttons[y][x] = tempButton;
+      System.out.println(tempButton.getActionCommand());
+      x++;
+      if (x >= boardSize) {
+        x = 0;
+        y++;
+      }
     }
     add(gameBoard);
     setSize(new Dimension(700, 700));
