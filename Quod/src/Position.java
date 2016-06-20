cc53d158d1b56e881e533cb5e8b5d96d9fd3a5b9
Manuel <wtfjoke@livenet.ch>
2016-06-20 16:18:39 +0200

    add equals and hashcode

--- Quod/src/Position.java
+++ Quod/src/Position.java
@@ -34,4 +34,24 @@ public class Position {
   public String toString() {
     return "" + positionX + "," + positionY;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + positionX;
+    result = prime * result + positionY;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null || getClass() != obj.getClass())
+      return false;
+    Position other = (Position) obj;
+    return positionX == other.getPositionX() && positionY == other.getPositionY();
+  }
+
 }
