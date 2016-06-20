57d113e5cef20d033faf4eac369a040c031bc191
Phi Long Tran <philongtran@outlook.de>
2016-06-20 07:36:46 +0200

    add method to calculate positions

--- Quod/src/QuodGame.java
+++ Quod/src/QuodGame.java
@@ -5,6 +5,8 @@ public class QuodGame extends Observable {
 
   private static QuodGame _game;
   private final Board board;
+  private Position[] player1Stones;
+  private Position[] player2Stones;
 
   private QuodGame() {
     board = new Board();
@@ -22,9 +24,27 @@ public class QuodGame extends Observable {
   }
 
   public void setBoard(int x, int y, int playerID) {
+    if (playerID == 1) {
+      player1Stones[player1Stones.length] = new Position(x, y);
+    } else if (playerID == 2) {
+      player2Stones[player2Stones.length] = new Position(x, y);
+    }
     board.setBoard(x, y, playerID);
+    positionCheck(playerID);
     setChanged();
     notifyObservers();
+  }
+
+  public void positionCheck(int playerID) {
+    int newX, newY;
+    if (playerID == 1 && player1Stones.length > 3) {
+      for (int i = 0; i < player1Stones.length; i++) {
+        player1Stones[i].getPositionX();
+        player1Stones[i].getPositionY();
+      }
+    } else if (playerID == 2 && player2Stones.length > 3) {
+
+    }
   }
 
 }
