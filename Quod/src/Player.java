04fb75da60d6147d1ff8364bf36c9a9e822cabc6
Phi Long Tran <philongtran@outlook.de>
2016-06-21 07:38:12 +0200

    add player methods and attributes for his stones

--- Quod/src/Player.java
+++ Quod/src/Player.java
@@ -3,6 +3,8 @@ import java.awt.Color;
 public class Player {
 
   private final Color color;
+  private int stones = 20;
+  private int greyStones = 6;
 
   public Player(Color color) {
     this.color = color;
@@ -15,5 +17,29 @@ public class Player {
   @Override
   public String toString() {
     return color.toString();
+  }
+
+  public boolean setStones() {
+    if (stones <= 0) {
+      return false;
+    }
+    stones--;
+    return true;
+  }
+
+  public int getStones() {
+    return stones;
+  }
+
+  public int getGreyStones() {
+    return greyStones;
+  }
+
+  public boolean setGreyStones() {
+    if (greyStones <= 0) {
+      return false;
+    }
+    greyStones--;
+    return true;
   }
 }
