69a66110e86d43282e0ccb9dd0ba573df865560a
Manuel <wtfjoke@livenet.ch>
2016-06-23 01:37:28 +0200

    remove h and v gaps

--- Quod/src/QuodBoardView.java
+++ Quod/src/QuodBoardView.java
@@ -8,7 +8,7 @@ public class QuodBoardView {
   public static JPanel asPanel(QuodBoard board) {
     JPanel gameBoard = new JPanel();
     int boardSize = board.getSize();
-    gameBoard.setLayout(new GridLayout(boardSize, boardSize, 1, 1));
+    gameBoard.setLayout(new GridLayout(boardSize, boardSize));
 
     for (int x = 0; x < boardSize; x++) {
       for (int y = 0; y < boardSize; y++) {
