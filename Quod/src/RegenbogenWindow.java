cedb145df5fcfa5a35db8921e894172bba1c7c9f
Phi Long Tran <philongtran@outlook.de>
2016-06-23 11:59:55 +0200

    add java doc

--- Quod/src/RegenbogenWindow.java
+++ Quod/src/RegenbogenWindow.java
@@ -5,16 +5,27 @@ import java.awt.event.ActionListener;
 import javax.swing.JInternalFrame;
 import javax.swing.Timer;
 
+/**
+ * This class changes it colors like a rainbow
+ * 
+ * @author Phi Long Tran <191624>
+ * @author Manuel Wessner <191711>
+ * @author Steve Nono <191709>
+ */
+
 public class RegenbogenWindow extends JInternalFrame implements ActionListener {
 
   /**
-   * 
+   * instance variables
    */
   private static final long serialVersionUID = 1L;
   Color[] colors =
       {Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.cyan, Color.magenta};
   int col = 0;
 
+  /**
+   * constructor set window attributes and add timer
+   */
   public RegenbogenWindow() {
     super("Rainbow", true, true);
     setSize(400, 400);
@@ -32,6 +43,9 @@ public class RegenbogenWindow extends JInternalFrame implements ActionListener {
     new Timer(delay, taskPerformer).start();
   }
 
+  /**
+   * change bg color on every timer event
+   */
   @Override
   public void actionPerformed(ActionEvent e) {
     col = (col + 1) % colors.length;
