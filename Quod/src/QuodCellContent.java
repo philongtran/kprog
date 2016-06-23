4e0587b8d8e7f829463468567d6b65b6e4e215f8
Phi Long Tran <philongtran@outlook.de>
2016-06-23 12:11:14 +0200

    add java doc

--- Quod/src/QuodCellContent.java
+++ Quod/src/QuodCellContent.java
@@ -1,4 +1,10 @@
-
+/**
+ * This enum is for the contents of a cell
+ * 
+ * @author Phi Long Tran <191624>
+ * @author Manuel Wessner <191711>
+ * @author Steve Nono <191709>
+ */
 public enum QuodCellContent {
   EMPTY, QUAD, QUASAR
 }
