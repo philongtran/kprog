373082b64884cb06ed0f9adba8a12c34d523f868
Phi Long Tran <philongtran@outlook.de>
2016-06-23 11:56:58 +0200

    add java doc

--- Quod/src/IconHelper.java
+++ Quod/src/IconHelper.java
@@ -4,10 +4,26 @@ import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+/**
+ * This class loads the menu icons and applies it to the menubar
+ * 
+ * @author Phi Long Tran <191624>
+ * @author Manuel Wessner <191711>
+ * @author Steve Nono <191709>
+ */
+
 public class IconHelper {
 
   private static final String IMAGEFILEEXTENSION = ".png";
 
+  /**
+   * loads the image from hdd
+   * 
+   * @param imageName
+   * @param width
+   * @param height
+   * @return
+   */
   public static ImageIcon getIcon(String imageName, int width, int height) {
     String image = imageName + IMAGEFILEEXTENSION;
     URL imagePath = IconHelper.class.getResource("resources/icons/" + image);
@@ -19,10 +35,23 @@ public class IconHelper {
     return null;
   }
 
+  /**
+   * this method creates an icon with an image and size
+   * 
+   * @param imageName
+   * @param button
+   * @return
+   */
   public static ImageIcon getIcon(String imageName, JButton button) {
     return getIcon(imageName, button.getWidth(), button.getHeight());
   }
 
+  /**
+   * this method applies an icon to a button
+   * 
+   * @param imageName
+   * @param button
+   */
   public static void setIcon(String imageName, JButton button) {
     button.setIcon(getIcon(imageName, button));
   }
